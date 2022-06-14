724881fb17f5ed5ca289b93fc80ebc75fad2d61b
pritesh511 <94275226+pritesh511@users.noreply.github.com>
2022-06-14 16:36:20 +0530

    PayloadTooLargeError: request entity too large error solve

--- server/index.js
+++ server/index.js
@@ -7,8 +7,15 @@ const pdfTemplate = require("./documents");
 const PORT = 4000;
 
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "50mb",
+    extended: true,
+    parameterLimit: 50000,
+  })
+);
 
 app.post("/create-pdf", (req, res) => {
   console.log(req.body);
